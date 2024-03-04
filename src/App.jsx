e90fcbad25afae653d8572725a7ddd9d0123aaa6
virghiu <virghiu@gmail.com>
2024-03-04 14:21:11 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import Home from './assets/components/Home'
+import Modal from './assets/components/Modal'
+import Sidebar from './assets/components/Sidebar'
+
+function App() {
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  )
+}
+
+export default App
